d00b1844cbadbe28806f2e859f7bc3c8df07be4e
Malak Wagieh <malak.wagieh252k@gmail.com>
2025-07-11 21:29:45 +0300

    added terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,41 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.24.0"
     }
   }
 }
 
-# Configure the AWS Provider
-provider "aws" {
-  region = "eu-north-1"
+provider "docker" {}
+
+resource "docker_network" "teamavail_net" {
+  name = "teamavail_network"
 }
+
+resource "docker_image" "coolestv" {
+  name = "coolestv"
+  build {
+    path    = "../"
+    dockerfile = "../Dockerfile"
+  }
+}
+
+resource "docker_container" "app" {
+  name  = "teamavail_app"
+  image = docker_image.coolestv.name
+  networks_advanced {
+    name = docker_network.teamavail_net.name
+  }
+
+  ports {
+    internal = 3000
+    external = 4040
+  }
+
+  env = [
+    "NODE_ENV=development",
+    "PORT=3000"
+  ]
+}
+
